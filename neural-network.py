a21ded22b9ce9d609def41a460f8bf32dbcdb985
Mrigesh Madaan <f2015756@pilani.bits-pilani.ac.in>
2017-11-25 16:40:46 +0530

    Some commenting and removing error in mnistTraining

--- neural-network.py
+++ neural-network.py
@@ -1,5 +1,6 @@
 import reader.reader as rd
 import matplotlib.pyplot as plt
+
 def plotComparison():
 	# To plot a comparison between convergence with and without gain Fuzzification
 	session = rd.session()
@@ -17,7 +18,7 @@ def plotComparison():
 	plt.savefig("PlotFor8.png")
 
 def plotComparison3():
-	# To plot a comparison between convergence with and without gain Fuzzification
+	# To plot convergence with eta and momentum Fuzzification
 	session = rd.session()
 	arr=rd.train2(session, "./test_pics/8.jpg", 8,False,True,True)
 	plt.plot(arr,label="With Momentum and ETA fuzzification")
@@ -27,30 +28,5 @@ def plotComparison3():
 	plt.legend()
 	plt.show()
 	plt.savefig("PlotFor8.png")
-
-def plotComparison2():
-	# To plot a comparison between convegence with gain fuzzification and without and with momentum over mnist data
-	session = rd.session()
-	arr=rd.provideMnistTraining(session, 10)
-	plt.plot(arr,label="With Gain Fuzzification")
-	# There is something wrong with this mnist training as convergence is some 80.something % and in the following iterations it drops down and settles at 80%
-	# did not check if this is true for the ramining two
-	
-	session2 = rd.session()
-	arr=rd.provideMnistTraining(session2, 10, False)
-	plt.plot(arr2,label="Without Gain Fuzzification")
-
-
-	session3 = rd.session()
-	arr=rd.provideMnistTraining(session3, 10, False,0.5)
-	plt.plot(arr2,label="With Momentum Constant")
-
-	plt.set_yLabel="convergence minimum"
-	plt.set_xlabel="Number of iterations"
-	plt.legend()
-	plt.show()
-	plt.savefig("PlotForMnistDataComparison.png")
-
-# print(rd.outputCharacter(session, "./test_pics/8.jpg"))
 
 plotComparison()
